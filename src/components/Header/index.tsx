0ee0e7a2e46c73f8866cda922473184d3b8625b8
Jay <f1skdev@gmail.com>
2024-11-25 20:43:17 +0100

    change color of subtitle to be more visible

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export function Header({ children }: HeaderProps) {
     <>
       <img alt='Logo' title='Logo' src='/thor/icon-192.png' height={64} width={64} className='mx-auto pb-4 pt-8'></img>
       <h1 className="font-bold text-center text-4xl pt-4 pb-1">Thor</h1>
-      <h4 className="font-bold text-center text-muted pb-4">First Responder Messages</h4>
+      <h4 className="font-bold text-center text-slate-500 pb-4">First Responder Messages</h4>
       {children}
     </>
   );

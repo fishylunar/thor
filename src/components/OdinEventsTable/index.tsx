93b8f22d1fca21cedab1fff0cb6f5ac0ba30f6ab
Jay <f1skdev@gmail.com>
2024-11-25 20:22:03 +0100

    Added Skeleton

--- src/components/OdinEventsTable/index.tsx
+++ src/components/OdinEventsTable/index.tsx
@@ -1,5 +1,6 @@
 import { OdinEvent, OdinEvents } from '@/logic/Odin';
 import { Table, TableBody, TableRow, TableCell, TableHeader } from "@/components/ui/table";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface OdinEventsTableProps {
   events: OdinEvents | null;
@@ -18,14 +19,35 @@ export function OdinEventsTable({ events }: OdinEventsTableProps) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {events && events.map((odinEvent: OdinEvent) => (
-            <TableRow key={odinEvent.timestamp}>
-              <TableCell>{odinEvent.responder}</TableCell>
-              <TableCell>{odinEvent.station}</TableCell>
-              <TableCell>{odinEvent.message}</TableCell>
-              <TableCell>{odinEvent.timestamp}</TableCell>
-            </TableRow>
-          ))}
+        {events ? (
+            events.map((odinEvent: OdinEvent) => (
+              <TableRow key={odinEvent.timestamp}>
+                <TableCell>{odinEvent.responder}</TableCell>
+                <TableCell>{odinEvent.station}</TableCell>
+                <TableCell>{odinEvent.timestamp}</TableCell>
+                <TableCell>{odinEvent.message}</TableCell>
+              </TableRow>
+            ))
+          ) : (
+            Array(20)
+              .fill(null)
+              .map((_, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Skeleton className="w-20 h-4" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="w-20 h-4" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="w-20 h-4" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="w-40 h-4" />
+                  </TableCell>
+                </TableRow>
+              ))
+          )}
         </TableBody>
       </Table>
     </>

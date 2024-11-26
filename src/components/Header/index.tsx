a345b9e43d13055487557fab2792817d2d9a9c3e
Jay <f1skdev@gmail.com>
2024-11-26 20:01:17 +0100

    url

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 export function Header({ children }: HeaderProps) {
   return (
     <>
-      <img alt='Logo' title='Logo' src='/thor/icon-192.png' height={64} width={64} className='mx-auto pb-4 pt-8'></img>
+      <img alt='Logo' title='Logo' src='icon-192.png' height={64} width={64} className='mx-auto pb-4 pt-8'></img>
       <h1 className="font-bold text-center text-4xl pt-4 pb-1">Thor</h1>
       <h4 className="font-bold text-center text-slate-500 pb-4">First Responder Messages</h4>
       {children}
